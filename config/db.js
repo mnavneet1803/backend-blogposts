d9b04c67def48a2a5a79447222c0d2193522a28c
navneet malhotra <mnavneet@1803.com>
2023-03-31 14:54:04 +0530

    db connection updated

--- config/db.js
+++ config/db.js
@@ -1,26 +1,45 @@
-let mysql = require('mysql');
+// let mysql = require('mysql');
 
-let connection = mysql.createConnection({
-    host: 'sql12.freesqldatabase.com',
-    user: 'sql12609609',
-    password: 'Ds7isUcZkc',
-    database: 'sql12609609'
-});
-connection.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
+// let connection = mysql.createConnection({
+//     host: 'sql12.freesqldatabase.com',
+//     user: 'sql12609609',
+//     password: 'Ds7isUcZkc',
+//     database: 'sql12609609'
+// });
+// connection.connect(function(err) {
+//     if (err) {
+//       return console.error('error: ' + err.message);
+//     }
   
 
-    console.log('Connected to the MySQL server.');
+//     console.log('Connected to the MySQL server.');
   
    
-    //     var sql = "CREATE TABLE post (postrId int PRIMARY KEY AUTO_INCREMENT , title varchar(255) NOT NULL , content varchar(255) NOT NULL , imageUrl varchar(255) , createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP)";
-    //     connection.query(sql, function (err, result) {
-    //       if (err) throw err;
-    //       console.log("Table created");
+//     //     var sql = "CREATE TABLE post (postrId int PRIMARY KEY AUTO_INCREMENT , title varchar(255) NOT NULL , content varchar(255) NOT NULL , imageUrl varchar(255) , createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP)";
+//     //     connection.query(sql, function (err, result) {
+//     //       if (err) throw err;
+//     //       console.log("Table created");
     
-    //   });
-})
+//     //   });
+// })
 
-module.exports= connection;
+// module.exports= connection;
+
+var mysql = require('mysql');
+
+    var connection  = mysql.createPool({
+        host     : 'sql12.freesqldatabase.com',
+        database : 'sql12609609',
+        user     : 'sql12609609',
+        password : 'Ds7isUcZkc'
+    });
+    for (var i=0; i<connection.length;i++) {
+        connection.getConnection(function(err, on) {
+          on.query( 'INSERT INTO ...', function(err, rows) {
+            console.log('Connected to the MySQL server.');
+            on.release();
+          });
+        });
+    }
+
+    module.exports= connection;
