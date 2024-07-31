9c81d0fe9203e4243959a70e4d615ea60b3d9794
mnavneet1803 <102963603+mnavneet1803@users.noreply.github.com>
2024-07-31 18:55:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,10 +3,14 @@
 var mysql = require('mysql');
 
     var connection  = mysql.createPool({
-        host     : 'sql12.freesqldatabase.com',
-        database : 'sql12609609',
-        user     : 'sql12609609',
-        password : 'Ds7isUcZkc'
+        // host     : 'sql12.freesqldatabase.com',
+        // database : 'sql12609609',
+        // user     : 'sql12609609',
+        // password : 'Ds7isUcZkc'
+        host     : 'localhost',
+        database : 'blogpost',
+        user     : 'root',
+        password : ''
     });
     for (var i=0; i<connection.length;i++) {
         connection.getConnection(function(err, on) {
